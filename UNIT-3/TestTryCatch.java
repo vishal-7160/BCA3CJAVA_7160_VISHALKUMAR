0c1749b2be7021973fa41a12f897e17c0ff4e00e
Vishal kumar <vk9503650@gmail.com>
2026-09-16 14:22:00 +0530

    Add files via upload

---
+++ UNIT-3/TestTryCatch.java
@@ -0,0 +1,18 @@
+public class TestTryCatch{
+	public static void main(String args[]){
+		String studentName="vishal";
+		System.out.println(studentName+"started math calculation...");
+		
+		try{
+			int totalmarks=100;
+			int subjects=0;
+			int result=totalmarks/ subjects;
+			
+			System.out.println("Result:"+result);
+		}catch (ArithmeticException e){
+			System.out.println("Exception Caught:division by zero is not allowed for" +studentName+"!");
+			System.out.println("System error detail:"+ e.getMessage());
+		}
+		System.out.println(studentName+"'s program continues execution smoothly!");
+	}
+}
---
+++ UNIT-3/Testhrow.java
@@ -0,0 +1,17 @@
+public class Testhrow{
+	static void checkvotingeligibility(int age, String studentname){
+		if(age <18){
+			throw new Exception(studentname+" is not eligible to vote(age must be 18+.");
+		}else{
+			System.out.println(studentname+" is eligible to vote!");
+		}
+	}
+	public static void main(String args[]){
+		try{
+			System.out.println("checking registration for vishal..");
+			checkvotingeligibility(16,"vishal");
+		}catch(Exception e){
+			System.out.println("caught Explicit Exception:"+e.getMessage());
+		}
+	}
+}
